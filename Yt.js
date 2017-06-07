28aa33160f88d2158bf3d12729adf4f25c85ff09
Nils Bergmann <nilsbergmann@noim.io>
2017-06-07 11:29:51 +0200

    Update Yt.js

--- Yt.js
+++ Yt.js
@@ -1,13 +1,13 @@
 const ytdl = require('ytdl-core');
 module.exports = function(ctx, cb) {
-  if (ctx.data.id || ctx.data.url) {
-    console.log(ctx.data.url || `http://www.youtube.com/watch?v=${ctx.data.id}`);
-    ytdl.getInfo(ctx.data.url || `http://www.youtube.com/watch?v=${ctx.data.id}`).then(result => {
+  if (ctx.data.id) {
+    console.log(`http://www.youtube.com/watch?v=${ctx.data.id}`);
+    ytdl.getInfo(`http://www.youtube.com/watch?v=${ctx.data.id}`).then(result => {
       cb(null, result);
     }).catch(e => {
       cb(new Error(e));
     });
   } else {
-    cb(new Error("You need to specify the url or the id parameter."));
+    cb(new Error("You need to specify the id parameter."));
   }
 };
