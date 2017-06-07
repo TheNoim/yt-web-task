1914c4f5be3c6e78b3ef6da64463bc9e3d280abb
Nils Bergmann <nilsbergmann@noim.io>
2017-06-07 11:29:09 +0200

    Update Yt.js

--- Yt.js
+++ Yt.js
@@ -1,6 +1,7 @@
 const ytdl = require('ytdl-core');
 module.exports = function(ctx, cb) {
   if (ctx.data.id || ctx.data.url) {
+    console.log(ctx.data.url || `http://www.youtube.com/watch?v=${ctx.data.id}`);
     ytdl.getInfo(ctx.data.url || `http://www.youtube.com/watch?v=${ctx.data.id}`).then(result => {
       cb(null, result);
     }).catch(e => {
